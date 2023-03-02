95291f14cc4bab0d8cbc94d481bc58664226727a
sidali djebline <sidali.djebline@outlook.com>
2023-03-02 23:24:15 +0100

    Add project files.

---
+++ WpfTest/Model/PackageTable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+namespace WpfTest.Model
+{
+    public class PackageTable
+    {
+
+        [PrimaryKey,AutoIncrement]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Weight { get; set; }
+
+
+    }
+}
---
+++ WpfTest/ViewModels/ShellViewModel.cs
@@ -0,0 +1,52 @@
+﻿using Caliburn.Micro;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO.Packaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfTest.Model;
+
+namespace WpfTest.ViewModels
+{
+    public class ShellViewModel: Screen
+    {
+
+
+        public BindableCollection<PackageTable> Packages { get; set; }
+
+        public ShellViewModel() { 
+        
+        Packages = new BindableCollection<PackageTable>();
+        
+        }
+
+
+
+
+        public void Save()
+        {
+
+            PackageTable package = new PackageTable();
+
+            using (SQLiteConnection connection = new SQLiteConnection(App.databasePath))
+            {
+                connection.CreateTable<PackageTable>();
+                connection.Insert(package);
+            }
+        }
+
+
+        public void Delete()
+        {   
+            PackageTable package = new PackageTable();
+            using (SQLiteConnection connection = new SQLiteConnection(App.databasePath))
+            {
+              //  connection.Delete<PackageTable>();
+             
+                connection.Delete(package);
+            }
+        }
+    }
+}
